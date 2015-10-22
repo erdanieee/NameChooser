683e8f2e62f831fa2f12a6a860d7dffbc01d94fa
Er Danié <erdanieee@users.noreply.github.com>
2015-10-22 13:37:43 +0200

    test

--- app/src/main/java/com/example/dan/selectordenombres/NameChooserActivity.java
+++ app/src/main/java/com/example/dan/selectordenombres/NameChooserActivity.java
@@ -68,7 +68,7 @@ public class NameChooserActivity extends AppCompatActivity implements View.OnCli
     //TODO: quantiles:  0%      10%     20%     30%     40%     50%     60%     70%     80%     90%     100%
     //TODO:             0.0040  0.0050  0.0060  0.0080  0.0110  0.0160  0.0260  0.0480  0.1018  0.3398  29.2160
     //TODO:             2437    2258    2048    1769    1489    1235    982     735     488     244     1
-    //////a
+
 
 
     @Override
